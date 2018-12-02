b082bc7d1bd8afe3dfcacf46a6f7e2f96f69d16a
benjamin bruyns <36083108+benji19@users.noreply.github.com>
2018-12-02 14:13:26 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -11,15 +11,7 @@
 </head>
 <body>
     <div class="container">
-    <?php echo "<h1>Welome to my database :D</h1>"; ?>
-		<table>
-			<tr><td><a href="/index.php">BMW_adres</a></td></tr>
-			<tr><td><a href="/tabel2.php">BMW_auto</a></td></tr>
-			<tr><td><a href="/tabel3.php">BMW_info</a></td></tr>
-			<tr><td><a href="/tabel4.php">BMW_land</a></td></tr>
-			<tr><td><a href="/tabel5.php">Shop_categorie</a></td></tr>
-			<tr><td><a href="/tabel6.php">Shop_leverancier</a></td></tr>
-		</table>
+    <?php echo "<h1>Welome to my database :p</h1>"; ?>
     <?php
 
     // Connexion et sélection de la base
